afaf6d41fd975de275d505bc3ff314d22e3091a2
unknown <chhantyalbipash@gmail.com>
2020-12-27 11:02:12 +0545

    Added

--- app/src/main/java/com/bipash/labsessionseven/MainActivity.kt
+++ app/src/main/java/com/bipash/labsessionseven/MainActivity.kt
@@ -1,30 +1,28 @@
 package com.bipash.labsessionseven
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var listView : ListView
     private lateinit var txtDetails : TextView
+    private lateinit var btnAdd : Button
 
-    private lateinit var getDetails : ArrayList<StudentDetails>
+    private var getDetails = arrayListOf<StudentDetails>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivityForResult(Intent(this, FormActivity::class.java), 1)
-
         listView = findViewById(R.id.listView)
         txtDetails = findViewById(R.id.txtDetails)
+        btnAdd = findViewById(R.id.btnAdd)
 
-        getDetails =intent.getSerializableExtra("receiveDetails") as ArrayList<StudentDetails>
 
         val adapter = ArrayAdapter(
             this,
@@ -35,8 +33,25 @@ class MainActivity : AppCompatActivity() {
         listView.adapter = adapter
 
         listView.setOnItemClickListener { parent, view, position, id ->
-            val showDetails =parent.getItemAtPosition(position).toString()
-            Toast.makeText(this, "$showDetails", Toast.LENGTH_SHORT).show()
+            txtDetails.text =parent.getItemAtPosition(position).toString()
+        }
+        btnAdd.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btnAdd ->{
+                startActivityForResult(Intent(this, FormActivity::class.java), 1)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode==1){
+            if(resultCode==Activity.RESULT_OK){
+                data?.getSerializableExtra("getDetails")
+            }
         }
     }
 }
